203f098c50a5339ae8fb7c7a0f50db18caa4a7b0
Boburjon Dadajonov <dadajonovboburjon3@gmail.com>
2022-01-09 22:16:53 +0500

    table for news

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import user_login, user_register, dashboard
+from .views import user_login, user_register, dashboard, disactive_news, delete_news, change_news, active_news
 
 urlpatterns = [
     path('log-in', user_login, name='login_url'),
     path('register', user_register, name='register_url'),
     path('log-out', user_register, name='logout_url'),
-    path('dashboard', dashboard, name='dashboard')
+    path('dashboard', dashboard, name='dashboard_url'),
+    path('disactive-news', disactive_news, name='disactive_news_url'),
+    path('active-news', active_news, name='active_news_url'),
+    path('delete-news', delete_news, name='delete_news_url'),
+    path('change-news/<int:pk>/', change_news, name='change_news_url')
 ]
--- account/views.py
+++ account/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 from.models import Staff, region
 from main.models import *
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+
+def PagenatorPage(List, num, request):
+    paginator = Paginator(List, num)
+    pages = request.GET.get('page')
+
+    try:
+        list = paginator.page(pages)
+    except PageNotAnInteger:
+        list = paginator.page(1)
+    except EmptyPage:
+        list = paginator.page(paginator.num_pages)
+    return list
 
 
 def user_login(request):
@@ -34,10 +49,44 @@ def dashboard(request):
         disactive_user = Staff.objects.filter(active=False).count()
         active_news = News.objects.filter(is_active=True).count()
         disactive_news = News.objects.filter(is_active=False).count()
-        context ={
-            'active_user':active_user,
-            'disactive_user':disactive_user,
-            'active_news':active_news,
-            'disactive_news':disactive_news
-        }
-        return render(request, 'account/admin-dashboard.html', context)
+    context ={
+        'active_user':active_user,
+        'disactive_user':disactive_user,
+        'active_news':active_news,
+        'disactive_news':disactive_news
+    }
+    return render(request, 'dashboard/admin-dashboard.html', context)
+
+
+
+def active_users_list(request):
+    return render(request, 'dashboard/active-users-list.html', {'user':PagenatorPage(Staff.objects.filter(active=True), 10, request)})
+
+def disactive_users_list(request):
+    return render(request, 'dashboard/disactive-users-list.html', {'user':PagenatorPage(Staff.objects.filter(active=False), 10, request)})
+
+def active_news(request):
+    return render(request, 'dashboard/active-news-list.html', {'news':PagenatorPage(News.objects.filter(is_active=True), 10, request)})
+
+def disactive_news(request):
+    return render(request, 'dashboard/disactive-news-list.html', {'news':PagenatorPage(News.objects.filter(is_active=False), 10, request)})
+
+
+def delete_news(request):
+    try:
+        new = request.GET.get('new_id')
+        delete_order = News.objects.get(id=new)
+        delete_order.delete()
+        return redirect('dashboard_url')
+    except:
+        return HttpResponse(False)
+
+def change_news(request, pk):
+    new = News.objects.get(id=pk)
+    active = request.POST.get('active')
+    if active == 'on':
+        new.is_active = True
+    else:
+        new.is_active = False
+    new.save()
+    return redirect('dashboard_url')
